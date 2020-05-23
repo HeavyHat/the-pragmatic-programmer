e9f2824d820d3b8c625c39975b5b241cd535a2b2
Joshua Langley <Joshua.langley.1995@gmail.com>
2020-05-23 15:06:48 +0100

    Updates

---
+++ precache-manifest.8f0106e8603d6ee07c55ae75101abde2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85682f899adb23fce2130b415181fb32",
+    "url": "/the-pragmatic-developer/index.html"
+  },
+  {
+    "revision": "907f1f159fdfe15a0e84",
+    "url": "/the-pragmatic-developer/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2ca5babf6d7e8e52baab",
+    "url": "/the-pragmatic-developer/static/js/2.0445a66a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/the-pragmatic-developer/static/js/2.0445a66a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "907f1f159fdfe15a0e84",
+    "url": "/the-pragmatic-developer/static/js/main.08e65bc3.chunk.js"
+  },
+  {
+    "revision": "a2a691e7dc2f3b4a9e44",
+    "url": "/the-pragmatic-developer/static/js/runtime-main.dad321b7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/the-pragmatic-developer/static/media/logo.5d5d9eef.svg"
+  }
+]);
